1251842a76aeb75c56d423525d63a4a37f6bf9b9
ArtKomarov <Komarov.A@phystech.edu>
2020-06-10 22:03:31 +0300

    Portals

--- hero.cpp
+++ hero.cpp
@@ -5,17 +5,35 @@
 
 const int JUMP_SPRITE_LEFT_OFFSET =  49 * 5 + 30;
 
+static sf::Clock JumpClock;
+
 Blocked::Blocked () :
     left_  (false),
     right_ (false),
     top_   (false),
     bot_   (false) {}
 
-Hero::Hero() :
+Blocked &Blocked::operator = (bool state) {
+    left_  = state;
+    right_ = state;
+    top_   = state;
+    bot_   = state;
+    return *this;
+}
+
+bool Blocked::operator == (bool state) {
+    return ((left_ == state) && (right_ == state) && (top_ == state) && (bot_ == state));
+}
+
+/// Constructors
+Hero::Hero(sf::Texture* globalTexture) :
     Speed_     (10),
     inJumping_ (0),
-    inFalling_ (false),
-    blocked_   () {
+    portals_{new Portal(BLUE, globalTexture), new Portal(ORANGE, globalTexture)} {
+
+    MoveAbility_ = HERO;
+    block_  = false;
+
     if(!texture_.loadFromFile("/home/artem/Main/Games/HeroRunSprite.png",
                                sf::IntRect(0, 0, 1000, 1000)
                                )) {
@@ -26,10 +44,12 @@ Hero::Hero() :
     }
 }
 
+// Unused
 Hero::Hero(const GrElem &hero) :
     texture_(dynamic_cast<const Hero &> (hero).texture_),
     sprite_ (dynamic_cast<const Hero &> (hero).sprite_),
-    lastMove_(1, 1) {}
+    portals_{dynamic_cast<const Hero &> (hero).portals_[0], dynamic_cast<const Hero &> (hero).portals_[1]},
+    lastMove_(1, 1){}
 
 Hero::Hero(const sf::Vector2f& position) :
     lastMove_(1, 1) {
@@ -41,14 +61,22 @@ Hero::Hero(const sf::Vector2f& position) :
 
     sprite_.setTexture     (texture_);
     sprite_.setTextureRect (sf::IntRect(0, 0, 49, 63));
-    sprite_.setPosition    (position);
+    this->SetPosition    (position);
 }
 
-Hero::~Hero() {}
-
-const sf::Vector2f Hero::GetLegPosition() {
-    return sf::Vector2f(sprite_.getPosition().x, sprite_.getPosition().y - 30);
+/// Destructor
+Hero::~Hero() {
+    delete portals_[0];
+    delete portals_[0];
 }
+
+Portal **Hero::GetPortals() {
+    return portals_;
+}
+
+//const sf::Vector2f Hero::GetLegPosition() {
+//    return sf::Vector2f(sprite_.getPosition().x, sprite_.getPosition().y - 30);
+//}
 
 //const sf::Sprite& Hero::GetSprite() const {
 //    return sprite_;
@@ -69,45 +97,130 @@ bool Hero::EventHendler(const sf::Event &event) {
     return catched;
 }
 
+bool Hero::MouseHendler(const sf::Event &event, sf::Vector2f MousePos) {
+    int PortNum = -1;
+    if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+        PortNum = 0;
+    else if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Right))
+        PortNum = 1;
+    else
+        return false;
+
+    std::cout << "PORT NUM: " << PortNum << std::endl;
+
+    portals_[PortNum]->SetMove(1);
+    if(portals_[!PortNum]->GetMoveSide() > 0)
+        portals_[!PortNum]->SetMove(-1);
+
+    for(int i = 0; i < 2; i++)
+        if(portals_[i]->GetMoveSide() != 0)
+            portals_[i]->SetPosition(sf::Vector2f(
+                                         this->GetPosition().x + this->GetGlobalBounds().width / 2,
+                                         this->GetPosition().y + this->GetGlobalBounds().height / 3 - 5)
+                                     );
+
+    for(int i = 0; i < 2; i++)
+        portals_[i]->SetSpeed(0, 0);
+    return false;
+}
+
 bool Hero::KeyPressedHandler(const sf::Event& event) {
     if(event.type != sf::Event::KeyPressed)
         return false;
+
+//    if(event.key.code == sf::Keyboard::W)
+//        std::cout << "W" << std::endl;
+//    if(event.key.code == sf::Keyboard::S)
+//        std::cout << "S" << std::endl;
+//    if(event.key.code == sf::Keyboard::A)
+//        std::cout << "A" << std::endl;
+//    if(event.key.code == sf::Keyboard::D)
+//        std::cout << "D" << std::endl;
+
     bool catched = false;
 
 
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-        this->SpriteMove(sf::Vector2f(Speed_, 0), false);
-        catched = true;
-    }
+//    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D) && !block_.right_) {
+//        std::cout << "D" << std::endl;
+//        block_ = false;
+//        this->SpriteMove(sf::Vector2f(Speed_, 0), false);
+//        catched = true;
+//    }
 
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-        this->SpriteMove(sf::Vector2f(-Speed_, 0), false);
-        catched = true;
-    }
+//    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A) && !block_.left_) {
+//        std::cout << "A" << std::endl;
+//        block_ = false;
+//        this->SpriteMove(sf::Vector2f(-Speed_, 0), false);
+//        catched = true;
+//    }
 
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-        this->SpriteMove(sf::Vector2f(0, -Speed_), false);
-        catched = true;
-    }
+//    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W) && inJumping_ == 0 && block_.bot_ && !block_.top_) { // key pressed + no double jump + earth + can jump
+//        JumpClock.restart();
+//        block_ = false;
+//        inJumping_ = 1;
+//        //this->SpriteMove(sf::Vector2f(0, -Speed_), false);
+//        catched = true;
+//    }
 
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-        this->SpriteMove(sf::Vector2f(0, Speed_), false);
-        catched = true;
-    }
+//    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S) && !block_.bot_) {
+//        block_ = false;
+//        this->SpriteMove(sf::Vector2f(0, Speed_), false);
+//        catched = true;
+//    }
 
 
     return catched;
 }
 
+void Hero::MousePressedHendler() {
+//    if(sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+//        portals_[0]->SetPosition(sf::Vector2f(
+//                                     this->GetPosition().x + this->GetGlobalBounds().width / 2,
+//                                     this->GetPosition().y + this->GetGlobalBounds().height / 3 - 5)
+//                                );
+//        portals_[0]->SetMove(1);
+
+//        portals_[0]->SetSpeed(0, 0);
+
+//        //portals_[0]->SetTarget(sf::Mouse::getPosition());
+
+////        sf::Vector2i mousePos = sf::Mouse::getPosition();
+
+////        float speedX = mousePos.x - this->GetPosition().x + this->GetGlobalBounds().width / 2;
+////        float speedY = mousePos.y - this->GetPosition().y + this->GetGlobalBounds().height / 3 - 5;
+
+////        float Sum = speedX + speedY;
+
+////        // Normalize * speed
+////        speedX = (speedX / Sum) * portals_[0]->GetSpeed();
+////        speedY = (speedY / Sum) * portals_[0]->GetSpeed();
+
+////        portals_[0]->SetSpeed(speedX, speedY);
+//    }
+}
+
 /// When move key not pressed, stop the hero (set proper sprite)
 bool Hero::KeyNotPressedHandler (const sf::Event& event) {
-    this->SpriteStop();
+    if(inJumping_ == 0)
+        this->SpriteStop();
     return false;
 }
 
 /// Set hero position
 void Hero::SetPosition (const sf::Vector2f &position) {
+    std::cout << __PRETTY_FUNCTION__ << ": " << position.x << " " << position.y << std::endl;
     sprite_.setPosition(position);
+    block_ = false;
+
+    //portals_[0]->SetMove(-1);
+    //portals_[1]->SetMove(-1);
+
+    portals_[0]->SetSpeed(0, 0);
+    portals_[1]->SetSpeed(0, 0);
+
+    std::cout << __PRETTY_FUNCTION__ << ": " << position.x << " " << position.y << std::endl;
+//    portals_[0]->SetPosition(position);
+//    portals_[1]->SetPosition(position);
 }
 
 /// Get hero position
@@ -120,48 +233,211 @@ sf::FloatRect Hero::GetGlobalBounds() const {
     return sprite_.getGlobalBounds();
 }
 
-/// Update hero
-int Hero::Update (sf::RenderTarget &target) {
-    if(inJumping_ > 0) {
-        if(--inJumping_ > 0)
-            this->SpriteMove(sf::Vector2f(0, -Speed_));
-        else {
-            this->SpriteStop();
-            inFalling_ = true;
-        }
-    }
-    else if(!blocked_.bot_) {
-        inFalling_ = true;
-        this->SpriteMove(sf::Vector2f(0, Speed_));
+/// Make some actions
+void Hero::Actions() {
+    //static sf::Clock ActClock;
+    //std::cout << inJumping_ << std::endl;
+
+    //this->MousePressedHendler();
+
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D) && !block_.right_) {
+        std::cout << "D" << std::endl;
+        block_ = false;
+
+        if(!inJumping_)
+            this->SpriteMove(sf::Vector2f(Speed_, 0), true, 0.05);
+        else
+            this->HeroMove(sf::Vector2f(Speed_, 0), true, 0.05);
+
     }
 
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A) && !block_.left_) {
+        std::cout << "A" << std::endl;
+        block_ = false;
+
+        if(!inJumping_)
+            this->SpriteMove(sf::Vector2f(-Speed_, 0), true, 0.05);
+        else
+            this->HeroMove(sf::Vector2f(-Speed_, 0), true, 0.05);
+
+    }
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W) && inJumping_ == 0 && block_.bot_ && !block_.top_) { // key pressed + no double jump + earth + can jump
+        std::cout << "W" << std::endl;
+        JumpClock.restart();
+        block_ = false;
+        inJumping_ = 1;
+        //this->SpriteMove(sf::Vector2f(0, -Speed_), false);
+    }
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S) && !block_.bot_) {
+        std::cout << "S" << std::endl;
+        block_ = false;
+
+        if(inJumping_)
+            inJumping_ = 0;
+
+        this->SpriteMove(sf::Vector2f(0, Speed_), true, 0.05);
+    }
+
+    if(inJumping_ > 0) {
+        if(JumpClock.getElapsedTime().asSeconds() > 0.5) {
+            inJumping_--;
+            JumpClock.restart();
+        }
+        if(inJumping_ > 0) {// catch moment of the higher position
+            std::cout << "Jumping" << std::endl;
+            this->SpriteMove(sf::Vector2f(0, -2 * Speed_));
+        }
+        else {
+            this->SpriteStop();
+            block_.bot_ = false;
+        }
+    }
+    else if(!block_.bot_) {
+        std::cout << "Falling" << std::endl;
+        block_.left_  = false;
+        block_.top_   = false;
+        block_.right_ = false;
+        this->HeroMove(sf::Vector2f(0, 2 * Speed_));
+    }
+
+    portals_[0]->Actions();
+    portals_[1]->Actions();
+
+    if(portals_[0]->GetGlobalBounds().intersects(this->GetGlobalBounds()))
+        if(portals_[0]->GetMoveSide() == 0 && portals_[1]->GetMoveSide() == 0) {
+            this->SetPosition(portals_[1]->GetPosition());
+            std::cout << __PRETTY_FUNCTION__ << ": portal move, new pos:"
+                      << this->GetPosition().x << " " << this->GetPosition().x << std::endl;
+        }
+}
+
+/// Update hero
+int Hero::Update (sf::RenderWindow &target) {
+    //portals_[0]->Update(target);
+    //portals_[1]->Update(target);
     return this->Draw(target);
 }
 
 /// Draw hero sprite
-int Hero::Draw (sf::RenderTarget &target) {
+int Hero::Draw (sf::RenderWindow &target) {
     target.draw(sprite_);
     return 0;
 }
 
-/// Move hero, move his sprite
-void Hero::SpriteMove(const sf::Vector2f& offset, bool withElapsed) { // TODO: Разбить по функциям!!!
+/// Default hero move (without moving sprite)
+void Hero::Move(sf::Vector2f offset) {
+    if(offset.x > 0 && block_.right_)
+        offset.x = 0;
+    if(offset.x < 0 && block_.left_)
+        offset.x = 0;
+    if(offset.y > 0 && block_.bot_)
+        offset.y = 0;
+    if(offset.y < 0 && block_.top_)
+        offset.y = 0;
+
+    sprite_.move(offset);
+}
+
+/// Special hero movement
+bool Hero::HeroMove(sf::Vector2f offset, bool withElapsed, float ElapsedSec) {
     // Set clock for checking elapsed time
-    static sf::Clock moveClock;
+    static sf::Clock moveLeftClock;
+    static sf::Clock moveRightClock;
+    static sf::Clock moveUpClock;
+    static sf::Clock moveDownClock;
 
     if(withElapsed) {
-        sf::Time elapsed = moveClock.getElapsedTime();
-        if(elapsed.asSeconds() < 0.1)
-            return;
-        moveClock.restart();
+
+        if(offset.x > 0) {
+            sf::Time elapsed = moveRightClock.getElapsedTime();
+
+            if(elapsed.asSeconds() < ElapsedSec) {
+                if(offset.y == 0)
+                    return false;
+                else
+                    offset.x = 0;
+            }
+            else
+                moveRightClock.restart();
+        } else if(offset.x < 0) {
+            sf::Time elapsed = moveLeftClock.getElapsedTime();
+
+            if(elapsed.asSeconds() < ElapsedSec) {
+                if(offset.y == 0)
+                    return false;
+                else
+                    offset.x = 0;
+            }
+            else
+                moveLeftClock.restart();
+        }
+
+        if(offset.y > 0) {
+            sf::Time elapsed = moveDownClock.getElapsedTime();
+
+            if(elapsed.asSeconds() < ElapsedSec) {
+                if(offset.x == 0)
+                    return false;
+                else
+                    offset.y = 0;
+            }
+            else
+                moveDownClock.restart();
+        } else if(offset.y < 0) {
+            sf::Time elapsed = moveUpClock.getElapsedTime();
+
+            if(elapsed.asSeconds() < ElapsedSec) {
+                if(offset.x == 0)
+                    return false;
+                else
+                    offset.y = 0;
+            }
+            else
+                moveUpClock.restart();
+        }
+
     }
 
-    // Move hero
-    sprite_.move(offset);
+    this->Move(offset);
+    return true;
+}
+
+/// Stop hero
+void Hero::Stop() {
+    //this->SpriteStop();
+}
+
+/// Move hero, move his sprite
+void Hero::SpriteMove(const sf::Vector2f& offset, bool withElapsed, float ElapsedSec) { // TODO: Разбить по функциям!!!
+    // Set clock for checking elapsed time
+
+    if(!this->HeroMove(offset, withElapsed, ElapsedSec))
+        return;
+
+//    if(offset.x > 0 && block_.right_)
+//        offset.x = 0;
+//    if(offset.x < 0 && block_.left_)
+//        offset.x = 0;
+//    if(offset.y > 0 && block_.bot_)
+//        offset.y = 0;
+//    if(offset.y < 0 && block_.top_)
+//        offset.y = 0;
+
+//    // Move hero
+//    sprite_.move(offset);
+
+    if(offset.x != 0 || offset.y != 0)
+        block_ = false;
+    else
+        return;
 
     // Select next texture rectangle (move sprite)
     if(offset.y < 0) {
-        if(!blocked_.top_) {
+        std::cout << "Move up" << std::endl;
+        if(!block_.top_) {
             int left;
 
             if(offset.x < 0 || (offset.x == 0 && lastMove_.x < 0))
@@ -173,19 +449,22 @@ void Hero::SpriteMove(const sf::Vector2f& offset, bool withElapsed) { // TODO: �
         }
         else {
             inJumping_ = 0;
-            inFalling_ = true;
         }
+        block_.bot_ = false;
     }
-    if(offset.x != 0) {
 
+    if(offset.x != 0) {
+        // Get number of current area in texture
         int CurrAreaNum;
         if(sprite_.getTextureRect().top == 0) {
             CurrAreaNum  = sprite_.getTextureRect().left / 49;
             CurrAreaNum -= (CurrAreaNum > 7);
+            CurrAreaNum += (CurrAreaNum < 2);
         }
         else {
             CurrAreaNum  = (texture_.getSize().x - sprite_.getTextureRect().left) / 49 - 1;
             CurrAreaNum -= (CurrAreaNum > 7);
+            CurrAreaNum += (CurrAreaNum < 2);
         }
 //        int left = (
 //                    CurrAreaNum * 49 +
@@ -229,29 +508,22 @@ void Hero::SpriteMove(const sf::Vector2f& offset, bool withElapsed) { // TODO: �
                 left = texture_.getSize().x - 49 - left;
         }
 
+        // Move sprite
         sprite_.setTextureRect(sf::IntRect(left, 63 * (offset.x < 0), 49, 63));
 
+        // Remember movement
         if(offset.x > 0)
             lastMove_.x = 1;
         else if(offset.x < 0)
             lastMove_.x = -1;
 
     }
-//    }
-//    else if(offset.x < 0) {
-//        int left = texture_.getSize().x - 49 - (texture_.getSize().x - sprite_.getTextureRect().left) % (49*5);
 
-//        if(lastMove_.x > 0)
-//            left = texture_.getSize().x - 49;
-
-//        sprite_.setTextureRect(sf::IntRect(left, 60, 49, 60));
-
-//        lastMove_.x = -1;
-//    }
 
 }
 
 void Hero::SpriteStop() {
+    std::cout << "Stop" << std::endl;
     // Set sprite stay area
     int left = 0;
     int top  = 0;
--- hero.h
+++ hero.h
@@ -3,15 +3,7 @@
 
 #include "SFML/Graphics.hpp"
 #include "graphics.h"
-
-struct Blocked {
-    bool left_;
-    bool right_;
-    bool top_;
-    bool bot_;
-
-    Blocked ();
-};
+#include "portal.h"
 
 class Hero : public GrElem {
     /// Graphics part
@@ -21,38 +13,44 @@ class Hero : public GrElem {
     /// Properties
     int  Speed_;
     int  inJumping_;
-    bool inFalling_;
-    Blocked blocked_;
+    Portal* portals_[2];
+    //bool inFalling_;
 
     /// Other
     sf::Vector2f lastMove_;
 
 public:
     /// Constructors/Destructor
-    Hero  ();
+    Hero  (sf::Texture* globalTexture);
     Hero  (const GrElem& hero);
     Hero  (const sf::Vector2f& position);
     ~Hero ();
 
     /// Accessors
-    const sf::Vector2f GetLegPosition();
     //const sf::Sprite& GetSprite() const;
+    Portal** GetPortals();
 
 
     /// Override graphical methods
     void          SetPosition     (const sf::Vector2f& position) override;
     sf::Vector2f  GetPosition     () const                       override;
     sf::FloatRect GetGlobalBounds () const                       override;
-    int           Update          (sf::RenderTarget&   target)   override;
-    int           Draw            (sf::RenderTarget&   target)   override;
+    void          Actions         ()                             override;
+    int           Update          (sf::RenderWindow&   target)   override;
+    int           Draw            (sf::RenderWindow&   target)   override;
+    void          Move            (sf::Vector2f        offset)   override;
+    void          Stop            ()                             override;
     bool          EventHendler    (const sf::Event&    event)    override;
+    bool          MouseHendler    (const sf::Event& event, sf::Vector2f MousePos) override;
 
     /// Personal hendlers
-    bool KeyPressedHandler(const sf::Event& event);
-    bool KeyNotPressedHandler(const sf::Event& event);
+    bool KeyPressedHandler    (const sf::Event& event);
+    void MousePressedHendler();
+    bool KeyNotPressedHandler (const sf::Event& event);
 
     /// Hero move ( to-to-to-to-to-toooooo :D )
-    void SpriteMove(const sf::Vector2f& offset, bool withElapsed = true);
+    bool HeroMove  (sf::Vector2f offset, bool withElapsed = true,  float ElapsedSec = 0.1);
+    void SpriteMove(const sf::Vector2f& offset, bool withElapsed = true, float ElapsedSec = 0.1);
     void SpriteStop();
 };
 
